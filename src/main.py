d7f308b715fc0ec935f8b2bdeab9596b408121ed
Volodymyr Vakhniuk <vvakhn2@uic.edu>
2020-04-17 21:03:48 -0500

    dsa
    sad

--- src/main.py
+++ src/main.py
@@ -1,13 +1,4 @@
 from Application import Application
 
-# import random
-
-# random.seed()
-
-# l = [random.randint(0, 3) for i in range(30)]
-# print(l)
-
-
-
-#app = Application()
-#app.runLoop()
+app = Application()
+app.runLoop()
